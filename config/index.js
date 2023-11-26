f46f7ee8e75b00fe9df6051015869d19bdb31268
jahdevelops <elijaheze777@gmail.com>
2023-11-26 01:00:58 -0600

    rename api key

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 module.exports = {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    CHAT_API_KEY: process.env.CHAT_API_KEY,
     DB_URL: process.env.DB_URL,
     PORT: process.env.PORT,
     USER_JWT_SECRET: process.env.USER_JWT_SECRET,
