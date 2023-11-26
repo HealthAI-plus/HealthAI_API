c1bb13dc15d83e6625e8854263a70fe9f08071b8
jahdevelops <elijaheze777@gmail.com>
2023-11-26 01:02:18 -0600

    add a messages attri for thread model

--- database/models/Thread.js
+++ database/models/Thread.js
@@ -6,6 +6,19 @@ const threadSchema = new mongoose.Schema({
         lowercase: true,
         default: ''
     },
+    external_ref: String,
+    messages: [{
+        generated_by: {
+            type: String,
+            enum: ['user', 'bot'],
+            required: true
+        },
+        content: String,
+        timestamp: {
+            type: Date,
+            default: Date.now()
+        } ,
+    }],
     tags: [String],
     prompts: [{type: mongoose.Types.ObjectId, ref: 'Prompt'}],
     user: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
