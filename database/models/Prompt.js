3c401a53f0e6b45059515760db88b99430100d98
jahdevelops <elijaheze777@gmail.com>
2023-11-27 07:34:47 -0600

    add message foreign key

--- database/models/Prompt.js
+++ database/models/Prompt.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const {CONSTANTS} = require('../../config')
 
 const promptSchema = new mongoose.Schema({
-   messages: [{type: mongoose.Types.ObjectId, ref: 'Message'}],
    tags: [String],
+   messages: [{type: mongoose.Types.ObjectId, ref: 'Message'}],
    thread: {type: mongoose.Types.ObjectId, ref: 'Thread', required: true},
    user: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
 }, {timestamps: true, autoIndex: false})
