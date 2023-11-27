5debe16a89ad15903e963230eb8f5b67f90b6b7e
jahdevelops <elijaheze777@gmail.com>
2023-11-27 07:34:28 -0600

    add message model

---
+++ database/models/Message.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose')
+const {CONSTANTS} = require('../../config')
+
+const messageSchema = new mongoose.Schema({
+   tags: [String],
+   type: {
+      type: String,
+      enum: [CONSTANTS.PROMPT.TYPES.TEXT, CONSTANTS.PROMPT.TYPES.SPEECH],
+      default: CONSTANTS.PROMPT.TYPES.TEXT
+   },
+   generated_by: {
+      type: String,
+      enum: [CONSTANTS.PROMPT.MESSAGE.GENERATED_BY_USER, CONSTANTS.PROMPT.MESSAGE.GENERATED_BY_BOT]
+   },
+   content: String,
+   thread: {type: mongoose.Types.ObjectId, ref: 'Thread', required: true},
+   prompt: {type: mongoose.Types.ObjectId, ref: 'Prompt', required: true},
+   user: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
+}, {timestamps: true, autoIndex: false})
+
+module.exports = mongoose.model('Message', messageSchema)
