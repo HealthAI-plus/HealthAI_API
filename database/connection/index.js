1e080ab650f1aa135d5563d7bdffc7cc24279bab
jahdevelops <mail>
2023-12-02 11:04:29 -0800

    set mongoose indexing to false

--- database/connection/index.js
+++ database/connection/index.js
@@ -4,7 +4,7 @@ const {DB_URL} = require('../../config')
 
 module.exports = () => {
     try {
-        mongoose.connect(DB_URL)
+        mongoose.connect(DB_URL, {autoIndex: false})
         logger.info("DB Connected.")
     } catch(err) {
         logger.error('DB failed to connect')
