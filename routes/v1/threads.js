46d2419401aa9839f28b27a12bab24c0d04f12ea
jahdevelops <mail>
2023-12-02 11:13:10 -0800

    add route handlers to share and delete threads

--- routes/v1/threads.js
+++ routes/v1/threads.js
@@ -3,6 +3,110 @@ var router = express.Router();
 const ThreadModel = require('../../database/models/Thread');
 const {validateUser} = require('./middlewares/users')
 const logger = require('../../logger')
+const {CONSTANTS, API_DOMAIN_NAME} = require('../../config');
+const crypto = require('crypto');
+const GeneratedLinkModel = require('../../database/models/GeneratedLink');
+
+router.delete('/:thread_id/delete', validateUser,  async (req, res) => {
+  const {userId} = req
+  const {thread_id} = req.params
+
+  try {
+    await ThreadModel.findByIdAndDelete(thread_id);
+    return res.status(204)
+    .json({
+      success: true,
+      message: 'Thread deleted.'
+    })
+  } catch (err) {
+    logger.error('Error while trying to delete thread', err)
+    return res.status(500)
+    .json({
+      success: false,
+      message: 'Could not delete thread'
+    })
+  }
+
+})
+
+router.post('/:thread_id/share', validateUser,  async (req, res) => {
+    const {userId} = req
+    const {thread_id} = req.params
+
+    let linkSlug,
+      encodedSlug,
+      sharableLink,
+      findThread;
+
+    if (!thread_id) {
+      return res.status(400)
+      .json({
+        success: false,
+        message: 'Invalid thread ID'
+      })
+    }
+
+    try {
+      findThread = await ThreadModel.findById(thread_id).select(['messages', 'title']);
+      if (!findThread) {
+        return res.status(404)
+        .json({
+          success: false,
+          message: 'Thread could not be found.'
+        })
+      }
+
+      if (findThread.messages.length === 0) {
+        return res.status(403)
+        .json({
+          success: false,
+          message: 'Cannot share an empty thread.'
+        })
+      }
+
+    } catch (err) {
+      logger.error('Error while trying to check for empty thread', err)
+      return res.status(500)
+      .json({
+        success: false,
+        message: 'Something went wrong'
+      })
+    }
+
+    try {
+        linkSlug = crypto.randomBytes(20).toString('base64');
+        encodedSlug =  encodeURIComponent(linkSlug);
+        sharableLink = `${API_DOMAIN_NAME}/invite/${encodedSlug}?ty=thread`;
+
+        await GeneratedLinkModel.create({
+            user: userId,
+            reason: CONSTANTS.GENERATED_LINK_REASON.SHARE_THREAD,
+            slug: linkSlug,
+            metadata: {
+              thread_snapshot: {
+                thread_title: findThread.title,
+                thread_messages: [...findThread.messages],
+              }
+            }
+        });
+        res.status(201)
+        .json({
+            success: true,
+            message: 'Link generated successfuly',
+            data: {
+                link: sharableLink
+            }
+        })
+    } catch(err) {
+        logger.error(err)
+        res.status(500)
+        .json({
+            success: false,
+            message: 'Could not generate sharable link'
+        })
+    }
+
+})
 
 router.get('/:thread_id', validateUser, async (req, res) => {
   const {userId} = req
